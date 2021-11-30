a536d0a4e4d4bc81b563361ffbb53347e9e99cca
14891 <1489163188@qq.com>
2021-11-30 14:47:29 +0800

    git 演示

---
+++ code/src/main/java/com/experiment/controller/CourseController.java
@@ -0,0 +1,58 @@
+package com.experiment.controller;
+
+import com.experiment.common.Result;
+import com.experiment.entity.Course;
+import com.experiment.entity.User;
+import com.experiment.service.CourseService;
+import com.experiment.service.MessageService;
+import com.experiment.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.Date;
+
+@Api(tags = "用户基本操作")
+@RestController
+public class CourseController extends BaseController{
+    @Resource
+    UserService userService;
+    @Resource
+    MessageService messageService;
+    @Resource
+    CourseService courseService;
+
+    @ApiOperation("根据学生id查课")
+    @PostMapping("/studentIdFindArrCourse")
+    public Result<?> studentIdFindCourse(@RequestBody User user){
+        return courseService.studentIdFindCourse(user);
+    }
+
+    @ApiOperation("根据时间查课")
+    @PostMapping("/TimeFindCourse")
+    public Result<?> TimeFindCourse(User user, Date date1, Date date2){
+        return courseService.TimeFindCourse(user,date1,date2);
+    }
+
+    @ApiOperation("增加课程")
+    @PostMapping("/addCourse")
+    public Result<?> addCourse(@RequestBody Course course){
+        return courseService.addCourse(course);
+    }
+
+    @ApiOperation("删除课程")
+    @PostMapping("/deleteCourse")
+    public Result<?> deleteCourse(@RequestBody Course course){
+        return courseService.deleteCourse(course);
+    }
+
+
+//    @ApiOperation("获取未读消息数量")
+//    @GetMapping("/getMessageNum")
+//    public Result<?> getMessageNum(){
+//        return messageService.getMessageNum(getUserId());
+//    }
+}
--- code/src/main/java/com/experiment/service/CourseService.java
+++ code/src/main/java/com/experiment/service/CourseService.java
@@ -1,4 +1,14 @@
 package com.experiment.service;
 
+import com.experiment.common.Result;
+import com.experiment.entity.User;
+import com.experiment.entity.Course;
+
+import java.util.Date;
+
 public interface CourseService {
+    Result<?> studentIdFindCourse(User user);
+    Result<?> addCourse(Course course);
+    Result<?> deleteCourse(Course course);
+    Result<?> TimeFindCourse(User user,Date date1,Date date2);
 }
